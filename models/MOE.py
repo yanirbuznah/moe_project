9b07f3eb22e4ec6161425168d150ab606f327e34
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2023-09-08 17:28:06 +0300

    fix augmentation

--- models/MOE.py
+++ models/MOE.py
@@ -160,7 +160,7 @@ class MixtureOfExperts(nn.Module):
                     nn.init.zeros_(layer.bias)
 
     def train_router(self, epoch):
-        if epoch % self.alternate:
+        if epoch % self.alternate == 0 and epoch > self.alternate:
             try:
                 self.router.learn()
             except Exception as e:
