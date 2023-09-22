bcbaf48379303b5f8def5bcbbc6f426df8f61ab5
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2023-09-22 16:23:26 +0300

    change imagenet location

--- datasets_and_dataloaders/utils.py
+++ datasets_and_dataloaders/utils.py
@@ -20,8 +20,8 @@ def get_dataset(name: str, train_dataset: bool):
         train = load_dataset('Maysee/tiny-imagenet', split='train')
         val = load_dataset('Maysee/tiny-imagenet', split='valid')
     elif name.lower() == 'imagenet':
-        train = torchvision.datasets.ImageFolder(root='/dsi/shared/datasets/imagenet/train', transform=None)
-        val = torchvision.datasets.ImageFolder(root='/dsi/shared/datasets/imagenet/train', transform=None)
+        train = torchvision.datasets.ImageFolder(root='/dsi/shared/yanir/imagenet/train', transform=None)
+        val = torchvision.datasets.ImageFolder(root='/dsi/shared/yanir/imagenet/val', transform=None)
     else:
         raise NotImplementedError(f"Dataset {name} not implemented")
     if train_dataset:
