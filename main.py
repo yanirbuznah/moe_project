264eac3b64e20a94c0f142448d776be2eebda8e5
yanir <yanirbuznah@gmail.com>
2024-06-14 14:12:22 +0300

    add multiple_configs option

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pickle
 import traceback
+from itertools import product
 from pprint import pformat
 import wandb
 
@@ -41,14 +42,25 @@ def main():
                 else:
                     nested_dict[k] = value
 
+
+
     if 'changes' in config:
-        for change in config['changes']:
-            for option in change['options']:
+        # Generate all combinations of options for each change
+        options_product = product(*(change['options'] for change in config['changes']))
+
+        # Iterate over each combination
+        for options in options_product:
+            # Set the values for each key-option pair
+            for change, option in zip(config['changes'], options):
                 set_value_by_key_from_nested_dict(config, change['key'], option)
 
-                if wandb_project_name is not None:
-                    wandb.init(project=wandb_project_name, config=config)
-                run_experiment(config)
+            # Initialize wandb if project name is provided
+            if wandb_project_name is not None:
+                wandb.init(project=wandb_project_name, config=config)
+            #
+            # Run the experiment with the current configuration
+            run_experiment(config)
+
     else:
         if wandb_project_name is not None:
             wandb.init(project=wandb_project_name, config=config)
